708d6fd5b5c68f370fefc791ec4fd3b008b851a8
MrGocuruk <96631537+MrGocuruk@users.noreply.github.com>
2021-12-24 21:10:42 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.showString("My")
+basic.showIcon(IconNames.Giraffe)
+basic.showString("is so...")
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
